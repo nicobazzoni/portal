9acba2eaac38ca01b84e504b6612dca201325af9
nico bazzoni <nicobazzoni@gmail.com>
2023-01-13 21:54:16 -0500

    back end connected to front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-
-import './App.css';
-import './style.scss'
-import {useState, useEffect} from 'react'
-import {Routes, Route, useNavigate} from 'react-router-dom'
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Detail from './pages/Detail';
-import About from './pages/About';
-import AddEditBlog from './pages/AddEditBlog';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Header from './components/Header';
-import Auth from './pages/Auth';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./style.scss";
+import "./media-query.css";
+import Home from "./pages/Home";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Detail from "./pages/Detail";
+import AddEditBlog from "./pages/AddEditBlog";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Auth from "./pages/Auth";
 import { auth } from "./firebase";
 import { signOut } from "firebase/auth";
-
+import TagBlog from "./pages/TagBlog";
+import CategoryBlog from "./pages/CategoryBlog";
+import ScrollToTop from "./components/ScrollToTop";
+import Blogs from "./pages/Blogs";
 
 function App() {
+  const [active, setActive] = useState("home");
+  const [user, setUser] = useState(null);
 
-  const [active, setActive] = useState("home")
-  const [user, setUser] = useState(null) 
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -42,17 +45,53 @@ function App() {
 
   return (
     <div className="App">
-      <Header setactive={setActive} active={setActive} user={user} handleLogout={handleLogout}/>
-      <ToastContainer position='top-center'/>
-   <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/detail/:id" element={<Detail/>}/>
-    <Route path="/create" element={<AddEditBlog/>}/>
-    <Route path="/about" element={<About/>}/> 
-    <Route path="/auth" element={<Auth setActive={setActive}/>}/>
-    <Route path="*" element={<NotFound/>}/>
-   
-   </Routes>
+      <Header
+        setActive={setActive}
+        active={active}
+        user={user}
+        handleLogout={handleLogout}
+      />
+      <ScrollToTop />
+      <ToastContainer position="top-center" />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home setActive={setActive} active={active} user={user} />}
+        />
+        <Route
+          path="/search"
+          element={<Home setActive={setActive} user={user} />}
+        />
+        <Route
+          path="/detail/:id"
+          element={<Detail setActive={setActive} user={user} />}
+        />
+        <Route
+          path="/create"
+          element={
+            user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />
+          }
+        />
+        <Route
+          path="/update/:id"
+          element={
+            user?.uid ? (
+              <AddEditBlog user={user} setActive={setActive} />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+        <Route path="/blogs" element={<Blogs setActive={setActive} />} />
+        <Route path="/tag/:tag" element={<TagBlog setActive={setActive} />} />
+        <Route path="/category/:category" element={<CategoryBlog setActive={setActive}  />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/auth"
+          element={<Auth setActive={setActive} setUser={setUser} />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
